6d07a6e66e9c6b26ce6bd93f08ac2f4b874f68b4
Ryo Fujimoto <fujimisakari@gmail.com>
2013-06-27 10:19:11 +0900

    ログインページ修正

--- urls.py
+++ urls.py
@@ -9,7 +9,7 @@ urlpatterns = patterns(
     url(r'^', include('module.top.urls')),
     url(r'^book/', include('module.book.urls')),
     url(r'^manage/', include('module.manage.urls')),
-    url(r'^login/$', 'django.contrib.auth.views.login', name='auth_login'),
+    url(r'^login/$', 'django.contrib.auth.views.login', kwargs={'template_name': 'root/login.html', 'extra_context': {'is_login_page': True}}, name='auth_login'),
     url(r'^logout/$', 'django.contrib.auth.views.logout_then_login', name='auth_logout'),
     url(r'^change_passwd/$', 'django.contrib.auth.views.password_change', kwargs={'post_change_redirect': 'change_passwd_done/'}, name='auth_password_change'),
     url(r'^change_passwd_done/$', 'django.contrib.auth.views.password_change_done', name='auth_password_change_done'),
