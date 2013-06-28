4d303f758b8fb82b067b5244ca40e1ac252da2d1
Ryo Fujimoto <fujimisakari@gmail.com>
2013-06-29 01:09:05 +0900

    パスワード変更

--- urls.py
+++ urls.py
@@ -11,7 +11,7 @@ urlpatterns = patterns(
     url(r'^manage/', include('module.manage.urls')),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login', kwargs={'template_name': 'root/login.html', 'extra_context': {'is_login_page': True}}, name='auth_login'),
     url(r'^logout/$', 'django.contrib.auth.views.logout_then_login', name='auth_logout'),
-    url(r'^change_passwd/$', 'django.contrib.auth.views.password_change', kwargs={'post_change_redirect': 'change_passwd_done/'}, name='auth_password_change'),
-    url(r'^change_passwd_done/$', 'django.contrib.auth.views.password_change_done', name='auth_password_change_done'),
+    url(r'^change_passwd/$', 'django.contrib.auth.views.password_change', kwargs={'template_name': 'root/password_change_form.html', 'extra_context': {'is_passward_page': True}}, name='auth_password_change'),
+    url(r'^change_passwd_done/$', 'django.contrib.auth.views.password_change_done', kwargs={'template_name': 'root/password_change_done.html', 'extra_context': {'is_passward_page': True}}, name='auth_password_change_done'),
     url(r'^admin/', include(admin.site.urls)),
 )
