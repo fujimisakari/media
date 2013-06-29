0927d0f1421253c0a75f79a10a8eee129f1aa582
Ryo Fujimoto <fujimisakari@gmail.com>
2013-06-30 02:55:47 +0900

    不要ファイルを削除

--- static.orig/js/import.js
+++
@@ -1,4 +0,0 @@
-document.write('<script type="text/javascript" src="/static/js/jquery-latest.js"></script>');  
-document.write('<script type="text/javascript" src="/static/js/script.js"></script>');  
-document.write('<script type="text/javascript" src="/static/js/jquery.switchHat.js"></script>');  
-document.write('<script type="text/javascript" src="/static/js/jquery.fancybox.pack.js"></script>');  
